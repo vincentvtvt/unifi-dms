062317f4b09f28738e5bb3e9108d7ecc39c551f2
vincentvtvt <wshengv@gmail.com>
2026-02-21 19:18:58 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Analytics } from '@vercel/analytics/react'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import App from './App';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+  return null;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <Analytics />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <ScrollToTop />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
